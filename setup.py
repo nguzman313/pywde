1d656cc69c9cbb87df9ac18fc4b8243e6386f674
Carlos Aya <carlosayam@users.noreply.github.com>
2018-05-30 22:03:59 +1000

    Add some basic stuff to improve package

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pywde',
+      version='0.0.1',
+      description='Wavelet-based density estimators',
+      author='Carlos Aya',
+      author_email='carlosayam@yahoo.com.au',
+      packages=['pywde'],
+     )
