d2641f589b06aa806491920e4cc2f5a55bd124d0
Carlos Aya <carlosayam@users.noreply.github.com>
2018-06-13 03:32:35 +1000

    Using f prime and f dual in tests; still bio2.4 failing (wip)

--- tests/pywt_ext_tests.py
+++ tests/pywt_ext_tests.py
@@ -6,58 +6,64 @@ from pywde.pywt_ext import Wavelet
 
 class WaveletCase(object):
 
-    def test_phi_prim(self):
-        self.assert_support(self.wave.phi_prim)
-        self.assert_norm(self.wave.phi_prim)
+    def test_fun_support(self):
+        a, b = self.fun.support
+        assert_array_almost_equal(np.zeros(6), self.fun(np.array([a - 0.5, a - 0.1, a, b, b + 0.1, b + 0.5])), 4)
 
-    def test_psi_prim(self):
-        self.assert_support(self.wave.psi_prim)
-        self.assert_norm(self.wave.phi_prim)
+    def test_dual_support(self):
+        a, b = self.fun_dual.support
+        assert_array_almost_equal(np.zeros(6), self.fun_dual(np.array([a - 0.5, a - 0.1, a, b, b + 0.1, b + 0.5])), 4)
 
-    def test_phi_dual(self):
-        self.assert_support(self.wave.phi_dual)
-        self.assert_norm(self.wave.phi_prim)
-
-    def test_psi_dual(self):
-        self.assert_support(self.wave.psi_dual)
-        self.assert_norm(self.wave.phi_prim)
-
-    @staticmethod
-    def assert_support(fun):
-        a, b = fun.support
-        assert_array_almost_equal(np.zeros(6), fun(np.array([a - 0.5, a - 0.1, a, b, b + 0.1, b + 0.5])), 4)
-
-    def assert_norm(self, fun):
-        a, b = fun.support
-        val_a = fun(np.linspace(a, b, num=int(10000*(b-a))))
-        self.assertAlmostEqual(1.0, (val_a * val_a).sum()/10000.0, 4)
+    def test_fun_dual_integral(self):
+        a1, b1 = self.fun.support
+        a2, b2 = self.fun_dual.support
+        a = min(a1,a2)
+        b = max(b1,b2)
+        val1 = self.fun(np.linspace(a, b, num=int(10000*(b-a))))
+        val2 = self.fun_dual(np.linspace(a, b, num=int(10000*(b-a))))
+        self.assertAlmostEqual(1.0, (val1 * val2).sum()/10000.0, 4)
 
 
-class TestDb2(unittest.TestCase, WaveletCase):
+class TestDb2Phi(unittest.TestCase, WaveletCase):
     def setUp(self):
-        self.wave = Wavelet('db2')
+        wave = Wavelet('db2')
+        self.fun = wave.phi_prim
+        self.fun_dual = wave.phi_dual
 
     def test_phi_prim_values(self):
         assert_array_almost_equal(np.array([1.3660254037844386, -0.3660254037844386]),
-                                  self.wave.phi_prim(np.array([1.0, 2.0])), 4)
+                                  self.fun(np.array([1.0, 2.0])), 4)
 
-
-class TestDb4(unittest.TestCase, WaveletCase):
+class TestDb2Psi(unittest.TestCase, WaveletCase):
     def setUp(self):
-        self.wave = Wavelet('db4')
-
-    def test_phi_prim_values(self):
-        assert_array_almost_equal(np.array([0.0, 1.301367, -0.736155, -0.060386]),
-                                  self.wave.psi_prim(np.array([-3.01, 0.66, 1.2, 2.1])), 4)
-
-    def test_psi_prim_values(self):
-        assert_array_almost_equal(np.array([0.0, 1.301367, -0.736155, -0.060386]),
-                                  self.wave.psi_prim(np.array([-3.01, 0.66, 1.2, 2.1])), 4)
+        wave = Wavelet('db2')
+        self.fun = wave.psi_prim
+        self.fun_dual = wave.psi_dual
 
 
-class TestBior24(unittest.TestCase, WaveletCase):
+class TestDb4Psi(unittest.TestCase, WaveletCase):
     def setUp(self):
-        self.wave = Wavelet('bior2.4')
+        wave = Wavelet('db4')
+        self.fun = wave.psi_prim
+        self.fun_dual = wave.psi_dual
+
+    def test_fun_values(self):
+        assert_array_almost_equal(np.array([0.0, 1.301367, -0.736155, -0.060386]),
+                                  self.fun(np.array([-3.01, 0.66, 1.2, 2.1])), 4)
+
+
+class TestBior24PhiPrim(unittest.TestCase, WaveletCase):
+    def setUp(self):
+        wave = Wavelet('bior2.4')
+        self.fun = wave.phi_prim
+        self.fun_dual = wave.phi_dual
+
+
+class TestBior24PsiPrim(unittest.TestCase, WaveletCase):
+    def setUp(self):
+        wave = Wavelet('bior2.4')
+        self.fun = wave.psi_prim
+        self.fun_dual = wave.psi_dual
 
 
 if __name__ == '__main__':
